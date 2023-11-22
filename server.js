77765143679fdefc1dea00dcc96af0f522efbaba
shtompee <101357568+shtompee@users.noreply.github.com>
2023-11-22 18:12:14 +0400

    zaza

--- server.js
+++ server.js
@@ -21,23 +21,108 @@ const pool = new Pool({
   ssl: true,
 });
 
-app.get("/", async function (req, res) {
-  res.writeHead(200, { "Content-Type": "application/json" });
+// app.get("/", async function (req, res) {
+//   res.writeHead(200, { "Content-Type": "application/json" });
 
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query("SELECT * FROM painting");
+//     const response = { response: result.rows };
+//     console.log(response);
+//     res.end(JSON.stringify(response));
+//     client.release();
+//   } catch (err) {
+//     console.error("Error executing query", err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
+app.get("/products", async (req, res) => {
   try {
-    const client = await pool.connect();
-    const result = await client.query(
-      "SELECT id, concat('data:image/jpeg;base64,', translate(encode(list_image, 'base64'), E'\n', '')) as list_image, name, prod_year, techlogy, paint_size, price, is_purchased FROM painting ORDER BY id ASC LIMIT $1 OFFSET $2"
-    );
-    const response = { response: result.rows };
-    console.log(response);
-    res.end(JSON.stringify(response));
-    client.release();
-  } catch (err) {
-    console.error("Error executing query", err);
-    res.status(500).send("Internal Server Error");
+    const page = req.query.page;
+
+    const itemsPerPage = req.query.itemsPerPage;
+
+    const prodcutsWrapper = await getProdcutsWrapper(page, itemsPerPage);
+
+    res.send(prodcutsWrapper);
+  } catch (error) {
+    console.error("Произошла ошибка:", error);
+    res.status(500).json({
+      error: "Произошла ошибка на сервере",
+    });
   }
 });
+
+//
+async function getProdcutsWrapper(page, itemsPerPage) {
+  try {
+    const [{ total }] = await getPaginatedCount();
+
+    const productList = await getPaginatedData(page, itemsPerPage);
+
+    const prodcutsWrapper = {
+      total: total,
+      productList: productList,
+    };
+
+    return JSON.stringify(prodcutsWrapper);
+  } catch (error) {
+    console.error("Произошла ошибка:", error);
+    return JSON.stringify({
+      error: "Произошла ошибка при получении данных",
+    });
+  }
+}
+
+function getPaginatedData(page, itemsPerPage) {
+  const limit = Number(itemsPerPage);
+  const offset = Number((page - 1) * itemsPerPage);
+
+  return new Promise((resolve, reject) => {
+    const query = `SELECT id, concat('data:image/jpeg;base64,', translate(encode(list_image, 'base64'), E'\n', '')) as list_image, name, prod_year, techlogy, paint_size, price, is_purchased FROM painting ORDER BY id ASC LIMIT $1 OFFSET $2`;
+
+    pool.connect((error, connection, release) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      connection.query(query, [limit, offset], (error, results) => {
+        release(); // Важно освободить соединение после выполнения запроса
+
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results.rows);
+        }
+      });
+    });
+  });
+}
+
+function getPaginatedCount() {
+  return new Promise((resolve, reject) => {
+    const query = `SELECT COUNT(*) AS total FROM painting`;
+
+    pool.connect((error, connection, release) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      connection.query(query, (error, results) => {
+        release(); // Важно освободить соединение после выполнения запроса
+
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results.rows);
+        }
+      });
+    });
+  });
+}
 
 // app.get("/", function (req, res) {
 //   res.writeHead(200, { "Content-Type": "application/json" });
