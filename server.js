4b2a2c48cb02950f19f716d32d009aa4bc784702
shtompee <101357568+shtompee@users.noreply.github.com>
2023-11-29 21:56:40 +0400

    zaza

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
   next();
 });
 
