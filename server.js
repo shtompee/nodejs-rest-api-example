f9aa372373cf94e901147d7d33e8ab68e68b6aa4
shtompee <101357568+shtompee@users.noreply.github.com>
2023-11-29 22:15:04 +0400

    zaza

--- server.js
+++ server.js
@@ -21,9 +21,14 @@ app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.sendFile(path.join(__dirname, "index.html"));
   next();
 });
+
+app.get("/"),
+  (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, "index.html"));
+  };
 
 //Serve static files from the "public" directory
 app.use(express.static("public"));
