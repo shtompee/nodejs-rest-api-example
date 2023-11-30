663000b226196e27ff8926c13b35af7c17050cf9
shtompee <101357568+shtompee@users.noreply.github.com>
2023-11-30 16:34:05 +0400

    zaza

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ var app = express();
 app.set("port", process.env.PORT || config.server.port);
 
 //Serve static files from the "public" directory
-app.use(express.static("public"));
+//app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "public", "index.html"))
+);
 
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
